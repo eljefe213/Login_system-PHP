8f07ee45f59ad9bb53a756866edaecb49a2f6d04
Hamaz Abdelghani <101653284+eljefe213@users.noreply.github.com>
2023-06-06 22:57:51 +0200

    style & body

--- header.php
+++ header.php
@@ -5,11 +5,34 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <title>Login System</title>
+
 </head>
 
 </body>
 
 <header>
-
+    <nav>
+        <a href="#">
+            <img src="img/logo.jpg" alt="logo">
+        </a>
+        <ul>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="#">Portfolio</a></li>
+            <li><a href="#">About me</a></li>
+            <li><a href="#">Contact</a></li>
+        </ul>
+        <div>
+            <form action="includes/login.inc.php" method="post">
+                <input type="text" name="mailuid" placeholder="Username/E-mail...">
+                <input type="text" name="pwd" placeholder="Password...">
+                <button type="submit" name="login-submit">Login</button>
+            </form>
+            <a href="signup.php">Sign up</a>
+            <form action="includes/login.inc.php" method="post">
+                <button type="submit" name="logout-submit">Logout</button>
+            </form>
+        </div>
+    </nav>
 </header>
--- index.php
+++ index.php
@@ -3,6 +3,8 @@ require "header.php";
 ?>
 
 <main>
+    <p>You are logged out!</p>
+    <p>You are logged in!</p>
 </main>
 
 <?php
